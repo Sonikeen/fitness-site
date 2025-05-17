cb5ab128f787deafc7e848067e57aa4647d94cd8
Sonikeen <sonyafdrv@gmail.com>
2025-05-17 12:04:09 +0300

    Update main.go

--- fitness-site/cmd/main.go
+++ fitness-site/cmd/main.go
@@ -4,27 +4,18 @@ import (
     "fmt"
     "log"
     "net/http"
-    "html/template"
+
+    "fitness-site/db"
+    "fitness-site/internal/router"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-    tmpl, err := template.ParseFiles("internal/templates/index.html")
-    if err != nil {
-        http.Error(w, "Template parsing error", http.StatusInternalServerError)
-        return
-    }
-    tmpl.Execute(w, nil)
-}
-
 func main() {
-    fs := http.FileServer(http.Dir("static"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
-    
-    http.HandleFunc("/", homeHandler)
+    // 1) Подключаемся к БД
+    db.Connect()
+
+    // 2) Настраиваем маршруты через ваш router
+    mux := router.SetupRoutes()
 
     fmt.Println("Сервер запущен на http://localhost:8080")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+    log.Fatal(http.ListenAndServe(":8080", mux))
 }
